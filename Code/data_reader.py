a02354fb9333df7d06a6a87bacfe0b2c9d836015
Dimitris Konstantineas <dimkonstantineas@gmail.com>
2025-08-26 18:14:47 +0300

    test

---
+++ Code/data_reader.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+train_dataset= pd.read_csv(r'data_files/train.csv')
+
+#print(train_dataset)
+#print(train_dataset['id'])
+
+total_missing_values_per_column=train_dataset.isnull().sum().sum()
+print(total_missing_values_per_column)
